c5342b715b5a39f9b643a3edfefa6df8be38e211
keysorbawah <keysorbawah@gmail.com>
2025-06-29 03:37:38 +0700

    Update site.ts

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
-export const nama = 'Smas Ngentot';
+export const nama = 'Sma Ngentot';
 export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
 export const url = 'https://smangentot.pages.dev';
 export const terbit = '2025-06-25T00:01:03Z';
